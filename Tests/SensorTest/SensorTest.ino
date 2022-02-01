5cca99acb6921e97daeb580b4ac8b10ec2a84ec9
Abhay <abhaymathur1000@gmail.com>
2022-01-31 22:49:00 -0500

    Initial Commit. Have samples for sensors and have test for different ones

---
+++ Tests/SensorTest/SensorTest.ino
@@ -0,0 +1,90 @@
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_BMP3XX.h"
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BMP3XX bmp;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+
+
+
+  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+}
+
+void loop() {
+  // Read normalized values 
+  Vector normAccel = mpu.readNormalizeAccel();
+  
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+
+  // Output
+  /*
+  Serial.print(" Pitch = ");
+  
+  Serial.print(pitch);
+  */
+  Serial.print(" Roll = ");
+  Serial.print(roll);
+
+  /*
+
+  
+  if (! bmp.performReading()) {
+    Serial.println("Failed to perform BMP reading :(");
+    return;
+  }
+  Serial.print("Temperature = ");
+  Serial.print(bmp.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  */
+
+
+  
+
+  
+
+  Serial.println();
+  delay(100);
+}
