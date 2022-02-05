7149f8cd88727359bd07ce15c02d002fe8316363
Abhay <abhaymathur1000@gmail.com>
2022-02-05 09:29:37 -0500

    Added the bno and GPS library
    Added these libraries and tested them out and they work. GPS takes a while to get running and bno is great

--- Tests/Prefabs/NRFTransmitter/NRFTransmitter.ino
+++ Tests/Prefabs/NRFTransmitter/NRFTransmitter.ino
@@ -1,29 +1,27 @@
-/*
-* Arduino Wireless Communication Tutorial
-*     Example 1 - Transmitter Code
-*                
-* by Dejan Nedelkovski, www.HowToMechatronics.com
-* 
-* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-*/
-
+//Include Libraries
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
+//create an RF24 object
+RF24 radio(9, 8);  // CE, CSN
 
+//address through which two modules communicate.
 const byte address[6] = "00001";
 
-void setup() {
+void setup()
+{
   radio.begin();
+  
+  //set the address
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
+  
+  //Set module as transmitter
   radio.stopListening();
 }
-
-void loop() {
+void loop()
+{
+  //Send message to receiver
   const char text[] = "Hello World";
   radio.write(&text, sizeof(text));
-  delay(1000);
 }
