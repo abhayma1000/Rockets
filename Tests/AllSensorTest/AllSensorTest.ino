50c82cc54f9f952f299a4f8dd8065564e933b998
Abhay <abhaymathur1000@gmail.com>
2022-02-05 09:30:05 -0500

    Updated allsensortest with new sensors
    Added BNO, GPS, and BMP to this test. It is working

--- Tests/AllSensorTest/AllSensorTest.ino
+++ Tests/AllSensorTest/AllSensorTest.ino
@@ -1,90 +1,154 @@
-
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <SPI.h>
 #include "Adafruit_BMP3XX.h"
-#include <MPU6050.h>
 
-MPU6050 mpu;
 
+//GPS setup
+SoftwareSerial mySerial(8, 7);
+Adafruit_GPS GPS(&mySerial);
+#define GPSECHO  true
+
+//BNO setup
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+
+//BMP setup
 #define BMP_SCK 13
 #define BMP_MISO 12
 #define BMP_MOSI 11
 #define BMP_CS 10
-
 #define SEALEVELPRESSURE_HPA (1013.25)
-
 Adafruit_BMP3XX bmp;
+
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial);
+  delay(5000);
 
-  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
-  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
-  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+
+  //GPS initialization
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+
+
+
+  //BNO initialization
+  if(!bno.begin())
+  {
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  bno.setExtCrystalUse(true);
+
+
+
+
+
+
+
+  //BMP initialization
+  if (!bmp.begin_I2C()) {
     Serial.println("Could not find a valid BMP3 sensor, check wiring!");
     while (1);
   }
-
-  // Set up oversampling and filter initialization
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
-
-
-
-
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
-  {
-    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
-    delay(500);
-  }
+  
+  
 }
 
+
+
+
+uint32_t timer = millis();
+
 void loop() {
-  // Read normalized values 
-  Vector normAccel = mpu.readNormalizeAccel();
-  
-  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
-  int roll = (atan2(normAccel.YAxis, normAccel.ZAxis)*180.0)/M_PI;
+  //GPS code
+  char c = GPS.read();
+  if ((c) && (GPSECHO))
+    Serial.write(c);
+  if (GPS.newNMEAreceived()) {
+    if (!GPS.parse(GPS.lastNMEA()))
+      return;
+  }
+  if (millis() - timer > 2000) {
+    timer = millis();
+    if (GPS.hour < 10) { Serial.print('0'); }
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    if (GPS.minute < 10) { Serial.print('0'); }
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    if (GPS.seconds < 10) { Serial.print('0'); }
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    if (GPS.milliseconds < 10) {
+      Serial.print("00");
+    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
+      Serial.print("0");
+    }
+    Serial.println(GPS.milliseconds);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitudeDegrees, 6);
+      Serial.print(", ");
+      Serial.println(GPS.longitudeDegrees, 6);
 
-  // Output
-  /*
-  Serial.print(" Pitch = ");
-  
-  Serial.print(pitch);
-  */
-  Serial.print(" Roll = ");
-  Serial.print(roll);
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    }
+  }
 
-  /*
 
-  
+
+
+
+
+  //BNO code
+  sensors_event_t event;
+  bno.getEvent(&event);
+  Serial.print("X: ");
+  Serial.print(event.orientation.x, 4);
+  Serial.print("\tY: ");
+  Serial.print(event.orientation.y, 4);
+  Serial.print("\tZ: ");
+  Serial.println(event.orientation.z, 4);
+
+
+
+
   if (! bmp.performReading()) {
-    Serial.println("Failed to perform BMP reading :(");
     return;
   }
   Serial.print("Temperature = ");
   Serial.print(bmp.temperature);
   Serial.println(" *C");
-
   Serial.print("Pressure = ");
   Serial.print(bmp.pressure / 100.0);
   Serial.println(" hPa");
-
   Serial.print("Approx. Altitude = ");
   Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
   Serial.println(" m");
-
-  */
-
-
-  
-
-  
-
   Serial.println();
-  delay(100);
+
+
+
+  
+  delay(1000);
+  
+
+
+
+  
 }
