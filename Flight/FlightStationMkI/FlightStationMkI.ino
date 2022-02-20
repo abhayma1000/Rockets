cc454999322efed4bb23ecda022d595ea744b3f3
Abhay <abhaymathur1000@gmail.com>
2022-02-20 10:21:27 -0500

    Added FlightStationMkI
    This is the first flight test with just a nano, button, and battery

---
+++ Flight/FlightStationMkI/FlightStationMkI.ino
@@ -0,0 +1,41 @@
+//Just Arduino Nano, button, and battery
+//Ones button is pressed, just blinks. Otherwise, doesn't do anything
+//Meant to show battery stays in
+
+int state = 0;
+
+int button_pin = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(button_pin, INPUT_PULLUP);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(state == 0) {
+    int button_state = digitalRead(button_pin);
+
+    if(button_state == LOW) {
+      //Start blinking
+      state = 1;
+    }
+  }
+
+  if(state == 1) {
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    delay(1000);
+
+    digitalWrite(LED_BUILTIN, LOW);
+
+    delay(1000);
+  }
+}
